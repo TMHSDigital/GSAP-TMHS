a267d2b7c8cda3d69fded6932497ecc1bd127841
T <154358121+TMHSDigital@users.noreply.github.com>
2024-08-17 12:13:29 -0400

    Update cdn-libs.js

--- cdn-libs.js
+++ cdn-libs.js
@@ -1,9 +1,15 @@
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    document.head.appendChild(script);
+}
+
 // GSAP and Plugins
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js"><\/script>');
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js"><\/script>');
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/Draggable.min.js"><\/script>');
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/MotionPathPlugin.min.js"><\/script>');
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js");
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js");
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/Draggable.min.js");
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/MotionPathPlugin.min.js");
 
 // Other Libraries
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"><\/script>');
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"><\/script>');
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js");
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js");
