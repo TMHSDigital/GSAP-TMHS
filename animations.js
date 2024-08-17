2abd78cbbbdcb5d1fffd271d16f2e878737abdd9
T <154358121+TMHSDigital@users.noreply.github.com>
2024-08-17 12:34:57 -0400

    add inertia and smooth scroll libraries

--- animations.js
+++ animations.js
@@ -12,3 +12,18 @@ gsap.to("#scroll-box", {
         scrub: true
     }
 });
+
+// Inertia Effect for Draggable
+Draggable.create("#scroll-box", {
+    type: "x,y",
+    inertia: true,
+    bounds: document.body
+});
+
+// Smooth Scroll
+document.querySelectorAll("nav a").forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        gsap.to(window, {duration: 1, scrollTo: {y: this.getAttribute("href"), offsetY: 70}});
+    });
+});
--- cdn-libs.js
+++ cdn-libs.js
@@ -9,6 +9,8 @@ loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js");
 loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js");
 loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/Draggable.min.js");
 loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/MotionPathPlugin.min.js");
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollToPlugin.min.js");
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/InertiaPlugin.min.js");
 
 // Other Libraries
 loadScript("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js");
