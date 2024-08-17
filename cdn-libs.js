3a6eea9fbe5e894623e231aea3c9d77dbf978626
T <154358121+TMHSDigital@users.noreply.github.com>
2024-08-17 12:12:34 -0400

    Create cdn-libs.js

---
+++ cdn-libs.js
@@ -0,0 +1,9 @@
+// GSAP and Plugins
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js"><\/script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js"><\/script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/Draggable.min.js"><\/script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/MotionPathPlugin.min.js"><\/script>');
+
+// Other Libraries
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"><\/script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"><\/script>');
